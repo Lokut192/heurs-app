42ff8d8601c36206faaa8ec911ee3c56bef99362
Lokut <luke.ostermann@gmail.com>
2025-05-16 15:27:09 +0200

    💄 style: Update dialog min width

--- src/_shared/components/ui/dialog/dialog.tsx
+++ src/_shared/components/ui/dialog/dialog.tsx
@@ -42,7 +42,7 @@ export const Dialog: React.FC<{
           <DialPanel
             transition
             className={twMerge(
-              'bg-base-100 rounded-box relative transform overflow-hidden px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95',
+              'bg-base-100 rounded-box relative min-w-sm transform overflow-hidden px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95',
               props.className ?? '',
             )}
           >
