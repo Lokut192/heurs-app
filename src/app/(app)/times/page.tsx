78ab0e823316fe13d822a33e145a013e0947e276
Lokut <luke.ostermann@gmail.com>
2025-05-23 15:55:10 +0200

    ✨ feat(times): Adding calendar view

--- src/app/(app)/times/page.tsx
+++ src/app/(app)/times/page.tsx
@@ -1,3 +1,6 @@
+import { faCalendar, faList } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
 import type { Metadata } from 'next';
 
 import { SaveTimeButton } from '@/modules/times/components/button/action/save-time-btn';
@@ -5,6 +8,7 @@ import { CurrentTimesTableNavButton } from '@/modules/times/components/button/na
 import { NextTimesTableNavButton } from '@/modules/times/components/button/nav/times-table/next-button';
 import { PreviousTimesTableNavButton } from '@/modules/times/components/button/nav/times-table/previous-button';
 import { TodayTimesTableNavButton } from '@/modules/times/components/button/nav/times-table/today-button';
+import TimesMonthCalendar from '@/modules/times/components/calendar/month/times-month-calendar';
 import { TimesTable } from '@/modules/times/components/table/times-table';
 import TimesTableProvider from '@/modules/times/providers/times-table.prov';
 
@@ -34,14 +38,21 @@ export default async function TimesIndex(_props: TimesIndexProps) {
   return (
     <>
       <h1 className="text-5xl/9 font-bold">Times</h1>
+      <div className="mt-10 flex w-full items-center justify-end">
+        <SaveTimeButton />
+      </div>
       <TimesTableProvider>
-        <div className="mt-10 flex flex-col gap-5">
-          <div className="flex w-full items-center justify-end">
-            <SaveTimeButton />
-          </div>
+        <TabGroup className="mt-5">
+          <div className="flex w-full items-center justify-between">
+            <TabList className="flex items-center">
+              <Tab className="btn data-[selected]:bg-base-100 first:rounded-l-box last:rounded-r-box flex items-center justify-center rounded-none">
+                <FontAwesomeIcon icon={faList} className="fa-lg" />
+              </Tab>
+              <Tab className="btn data-[selected]:bg-base-100 first:rounded-l-box last:rounded-r-box flex items-center justify-center rounded-none">
+                <FontAwesomeIcon icon={faCalendar} className="fa-lg" />
+              </Tab>
+            </TabList>
 
-          <div className="flex w-full items-center justify-between gap-4">
-            <CurrentTimesTableNavButton />
             <div className="flex items-center justify-normal gap-2">
               <PreviousTimesTableNavButton className="btn-soft" />
               <TodayTimesTableNavButton className="btn-soft" />
@@ -49,8 +60,19 @@ export default async function TimesIndex(_props: TimesIndexProps) {
             </div>
           </div>
 
-          <TimesTable />
-        </div>
+          <div className="mt-3">
+            <CurrentTimesTableNavButton />
+          </div>
+
+          <TabPanels className="mt-3">
+            <TabPanel unmount={false}>
+              <TimesTable />
+            </TabPanel>
+            <TabPanel unmount={false}>
+              <TimesMonthCalendar />
+            </TabPanel>
+          </TabPanels>
+        </TabGroup>
       </TimesTableProvider>
     </>
   );
